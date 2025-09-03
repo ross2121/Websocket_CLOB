e2406c3b62d8c38f26cbe5dcff5f1ed9ae7d6832
Youval Singh <youvalsingh40@gmail.com>
2025-09-03 23:41:49 +0530

    impl:Ws completed

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,33 @@ mod types;
 mod Subscription_manager;
 mod user;
 mod usermanager;
+
+use futures_util::stream;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::accept_async;
+
+
 use types::IncomingMessage;
 
-fn main() {
-    println!("WebSocket Server Starting...");
+use crate::usermanager::Usermanager;
+#[tokio::main]
+async  fn main() {
+   let listner=TcpListener::bind("127.0.0.9001").await.unwrap();
+   while let Ok((Stream,_)) =  listner.accept().await{
+    tokio::spawn(handle_connection(Stream));
+   }
     
-    // Example usage of the types
-    let subscribe_msg = IncomingMessage::Subscribe(types::SubscribeMessage {
-        method: "SUBSCRIBE".to_string(),
-        params: vec!["btcusdt@ticker".to_string()],
-        id: 1,
-    });
-    
-    println!("Subscribe message: {:?}", subscribe_msg);
+ 
+}
+async fn handle_connection(stream:TcpStream){
+    match accept_async(stream).await {
+        Ok(ws_Stream)=>{
+            let user_manage=Usermanager::instance();
+            let mut manager_grd=user_manage.lock();
+            manager_grd.await.add_user(ws_Stream);
+        }
+        Err(e)=>{
+            println!("{}",e);
+        }
+    }
 }
